ba1157328655643b1e7df6c9c682b1bdc17cfa85
Anton <antemionoy@gmail.com>
2018-09-13 18:40:00 +0300

    add two sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,106 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/owl.carousel/dist/owl.carousel.js
 //= ../../bower_components/slick-carousel/slick/slick.js
+//= ../../bower_components/ion.rangeSlider/js/ion.rangeSlider.js
+//= ../../bower_components/magnific-popup/dist/jquery.magnific-popup.js
+//= ../../bower_components/sly/dist/sly.min.js
 
 "use strict";
+
+
+function zoomPopUp() {
+    $('.zoom-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+
+    });
+}
+
+function owlFun(sliderName, items, rangeItem, auto_width) {
+
+    var itemsNum = 0;
+
+    var owl = $(sliderName);
+
+    owl.on('initialized.owl.carousel', function(event) {
+        var itemCount = event.item.count;
+        var size = event.page.size;
+        var dragLength = 100 / (itemCount / size);
+
+        $(rangeItem).ionRangeSlider({
+            type: "single",
+            min: 1,
+            max: itemCount - (size - 1),
+            keyboard: true,
+            step: 1,
+            onChange: function(data) {
+                // owl.trigger('changed.owl.carousel', [???]);
+                owlTo = (data.from) - 1;
+
+                console.log("Позиция ползунка: " + owlTo);
+                owl.trigger('to.owl.carousel', [owlTo, 500, true]);
+
+            }
+        });
+
+        $('.irs-slider.single').css('width', dragLength + "%");
+    });
+
+
+    //Слайдер
+    owl.owlCarousel({
+        loop: false,
+        margin: 30,
+        nav: true,
+        dots: false,
+        slideBy: 1,
+        items: items,
+        autoWidth: auto_width,
+        center: false,
+        responsiveClass: true
+    });
+
+    console.log('sliderItem');
+
+
+    // owl.on('dragged.owl.carousel', function(event) {
+    //     var itemCount = event.item.count;
+    //     var size = event.page.size;
+    //     var curItem = event.item.index + 1;
+    //     var dragLength = 100 / (itemCount / size);
+    //     console.log(curItem);
+    //     $("#range").data("ionRangeSlider").update({ from: curItem });
+    //     $('.irs-slider.single').css('width', dragLength + "%");
+    // });
+
+    // owl.on('resized.owl.carousel', function(event) {
+    //     var itemCount = event.item.count;
+    //     var size = event.page.size;
+    //     var curItem = event.item.index + 1;
+    //     var dragLength = 100 / (itemCount / size);
+    //     $("#range").data("ionRangeSlider").update({
+    //         max: itemCount - (size - 1),
+    //         from: curItem
+    //     });
+    //     $('.irs-slider.single').css('width', dragLength + "%");
+    // });
+}
 
 function init_map() {
     var myOptions = {
@@ -22,32 +121,6 @@ function init_map() {
     });
 }
 
-
-function qa() {
-
-    $('.qa__button').on('click', function(e) {
-        e.preventDefault();
-
-        var findEl = $(this).parent().parent();
-
-        $('.qa__circle').removeClass('qa__circle--open');
-        $('.qa__full').removeClass('qa__full--open');
-
-        findEl.find('.qa__circle').addClass('qa__circle--open');
-        findEl.find('.qa__full').addClass('qa__full--open');
-
-    });
-
-    $('.qa__button--curtail').on('click', function(e) {
-        e.preventDefault();
-
-        $(this).parent().parent().find('.qa__circle').removeClass('qa__circle--open');
-        $(this).parent().parent().find('.qa__full').removeClass('qa__full--open');
-
-    });
-}
-
-
 function menu() {
 
     $(".hamburger").click(function() {
@@ -63,15 +136,25 @@ function menu() {
 
 $(function() {
     menu();
-    qa();
+    zoomPopUp();
+
+    owlFun('.reviews__slider', 4, '.reviews__input_range', false);
+    owlFun('.achieve__slider', 3, '.achieve__input_range', false);
 
     if (typeof(google) != 'undefined') {
         google.maps.event.addDomListener(window, 'load', init_map);
     }
 
     $('.clients__slider').slick({
-          slidesToShow: 6
+        slidesToShow: 6,
+        autoplay: true,
+        autoplaySpeed: 1500
     });
+
+    // $('.clients__slider').owlCarousel({
+    //     items: 6,
+    //     autoplay: true
+    //  });
 
     $('.js-scrollToEl').on('click', function(e) {
         e.preventDefault();
@@ -83,5 +166,4 @@ $(function() {
         }, 800);
 
     });
-
 });
